e62b69949c49be18bf12ab2740519cd30e5d47eb
slngvitoria@gmail.com <slngvitoria@gmail.com>
2022-02-06 15:50:59 -0300

    first commit

---
+++ script2.js
@@ -0,0 +1,91 @@
+const urlDoBack = "http://localhost:8000"; //rodando local
+
+function entrar() {
+    let login = document.getElementById("login");
+    let cadastrar = document.getElementById("cadastro");
+    login.style.display = "flex";
+    cadastrar.style.display = "none";
+
+    document.getElementById("userNameLogin").value = "";
+    document.getElementById("senhaLogin").value = "";
+}
+
+function logando () {
+    let nomeLogin = document.getElementById("userNameLogin").value;
+    let senhaLogin = document.getElementById("senhaLogin").value;
+
+    if (!nomeLogin || !senhaLogin) {
+        alert("Preencha todos os dados.");
+
+    } else {
+        axios.post(`${urlDoBack}/login`, {
+            nome: nomeLogin,
+            senha: senhaLogin,
+
+        }).then(resposta => {
+            localStorage.setItem("usuarioLogadoGrowdev", JSON.stringify(resposta.data.usuario));
+            window.location.href = "index3.html";
+
+        }) .catch((err) => {
+            alert(err.response.data)
+        })
+    }
+}
+
+
+function cadastrar() {
+    let login = document.getElementById("login");
+    let cadastrar = document.getElementById("cadastro");
+    login.style.display = "none";
+    cadastrar.style.display = "flex";
+
+    document.getElementById("userNameCadastro").value = "";
+    document.getElementById("senhaCadastro").value = "";
+    document.getElementById("senhaConfirmacaoCadastro").value = ""; 
+}
+
+entrar();
+
+function criarNovoUsuario () {
+    let nome = document.getElementById("userNameCadastro").value;
+    let senha = document.getElementById("senhaCadastro").value;
+    let confirmaçaoSenha = document.getElementById("senhaConfirmacaoCadastro").value;
+    
+    if (nome == "" || senha == "" || confirmaçaoSenha == "") {
+        alert ("Por favor preencha todos os dados");
+
+    } else {
+
+        if (senha == confirmaçaoSenha) {
+            
+            axios.post(`${urlDoBack}/cadastro`, {
+                nome: nome,
+                senha: senha,
+                
+                }).then((res) => {
+                    nome.value = "";
+                    senha.value = "";
+                
+                        limpaCadastro()
+                        // buscarComentarios()
+                        alert(res.data)
+                
+                }).catch((err) => {
+                    alert(err.response.data)
+                })
+            
+        } else {
+            alert("Senhas não conferem");
+        }
+    }
+}
+
+function limpaCadastro() {
+    document.getElementById("userNameCadastro").value = "";
+    document.getElementById("senhaCadastro").value = "";
+    document.getElementById("senhaConfirmacaoCadastro").value = "";
+}
+
+
+
+
---
+++ script3.js
@@ -0,0 +1,118 @@
+let usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogadoGrowdev"));
+const urlDoBack = "http://localhost:8000"; //rodando local
+
+function salvarRecado (event) {
+    event.preventDefault();
+
+    let salvarDescricao = document.getElementById("descricao").value;
+    let salvarDetalhamento = document.getElementById("detalhamento").value;
+
+    if (!salvarDescricao || !salvarDetalhamento) {
+        alert("Preencha todos os campos");
+
+    } else {
+        console.log("oi")
+        axios.post(`${urlDoBack}/recado`, {
+            usuario: usuarioLogado.id, 
+            descricao: salvarDescricao,
+            detalhamento: salvarDetalhamento,
+            
+        }).then((res) => {
+            console.log(res)
+            alert(res.data)
+            listarRecados();
+            
+        }).catch((err) => {
+            alert(err.response.data)
+        })
+    }
+}
+
+function paginaInicial () {
+    window.location.href = "index2.html";
+}
+
+function listarRecados () {
+
+    axios.get(`${urlDoBack}/listarecados/${usuarioLogado.id}`).then(function(res) {
+        let tbody = document.getElementById("tbody");
+        tbody.innerHTML = "";
+        console.log('res',res)
+        res.data.forEach(function(recado, index) {
+            let tr = document.createElement("tr");
+            let td1 = document.createElement("td");
+            let td2 = document.createElement("td");
+            let td3 = document.createElement("td");
+            let td4 = document.createElement("td");
+            let botaoExcluir = document.createElement("button")
+            let botaoEditar = document.createElement("button");
+
+            td1.innerHTML = recado.id;
+            td2.innerHTML = recado.descricao;
+            td3.innerHTML = recado.detalhamento;
+
+            botaoExcluir.className = "btn btn-danger";
+            botaoExcluir.innerText = "EXCLUIR";
+            botaoEditar.className = "btn btn-info";
+            botaoEditar.innerHTML = "EDITAR";
+            botaoEditar.style.marginLeft = "5px";
+            botaoExcluir.onclick = () => excluirRecado(recado.id); //cria função que executa exclusao
+            botaoEditar.onclick = () => editarRecado(recado);
+
+            td4.appendChild(botaoExcluir);
+            td4.appendChild(botaoEditar);
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            
+            tbody.appendChild(tr);
+        })
+    }).catch((err) => {
+        alert(err.response.data)
+        
+    })
+}
+
+function excluirRecado (idRecado) {
+
+    axios.delete(`${urlDoBack}/recado/${idRecado}`).then((res) => {
+        alert(res.data)
+
+    }).catch((err) => {
+        alert(err.response.data)
+    })
+
+    listarRecados();
+    
+}
+
+function editarRecado (recado) {
+    let novaDescricao = prompt("Nova Descrição:", recado.descricao );
+    if (novaDescricao == null || novaDescricao == "") {
+        return;
+    }
+    let novoDetalhamento = prompt("Novo Detalhamento:", recado.detalhamento );
+    if (novoDetalhamento == null || novoDetalhamento == "") {
+        return;
+    }
+
+    axios.put(`${urlDoBack}/recado/${recado.id}`, {
+        descricao: novaDescricao,
+        detalhamento: novoDetalhamento
+
+    }).then((res) => {
+        alert(res.data)
+
+    }).catch((err) => {
+        alert(err.response.data)
+    })
+
+    listarRecados();
+}
+
+
+listarRecados();
+
+
